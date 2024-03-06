889a074b9b83460d8c2571bddfd7e951feb28698
sametcanugur <sametcanugur@localhost>
2024-03-06 16:26:37 +0300

    Mean of number's digit is found.

---
+++ JavaAlgorithmProblems/src/Exercises/FindtheMeanofAllDigits.java
@@ -0,0 +1,18 @@
+package Exercises;
+
+public class FindtheMeanofAllDigits {
+
+	public static void main(String[] args) {
+		System.out.println(findMeanOfNumber(12345));
+	}
+
+	public static int findMeanOfNumber(int inputNumber) {
+		String convertedString=String.valueOf(inputNumber);
+		String[] splitArray=convertedString.split("");
+		int sum=0;
+		for(int i=0; i<splitArray.length; i++) {
+			sum=sum+Integer.valueOf(splitArray[i]);
+		}
+		return sum/splitArray.length;
+	}
+}
